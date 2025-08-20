178918c01d81b91e957698958de85be9f1757cea
João Pedro <jpappelt10@gmail.com>
2025-08-20 20:08:26 -0300

    Update Pokemon.java

--- TrabalhoFinalFS/src/main/java/Model/Pokemon.java
+++ TrabalhoFinalFS/src/main/java/Model/Pokemon.java
@@ -7,19 +7,24 @@ public class Pokemon {
     private String tipoSecundario; // Pode ser null
     private int nivel;
     private int hpMaximo;
+    private int FK_Treinador;
 
-    public Pokemon(int id, String nome, String tipoPrimario, String tipoSecundario, int nivel, int hpMaximo) {
+
+    
+
+    public Pokemon(int id, String nome, String tipoPrimario, String tipoSecundario, int nivel, int hpMaximo, int FK_Treinador) {
         this.id = id;
         this.nome = nome;
         this.tipoPrimario = tipoPrimario;
         this.tipoSecundario = tipoSecundario;
+        this.FK_Treinador = FK_Treinador;
         // As validações são chamadas aqui pelos setters
         setNivel(nivel);
         setHpMaximo(hpMaximo);
     }
 
-    public Pokemon(String nome, String tipoPrimario, String tipoSecundario, int nivel, int hpMaximo) {
-        this(-1, nome, tipoPrimario, tipoSecundario, nivel, hpMaximo);
+    public Pokemon(String nome, String tipoPrimario, String tipoSecundario, int nivel, int hpMaximo,int FK_Treinador) {
+        this(-1, nome, tipoPrimario, tipoSecundario, nivel, hpMaximo, FK_Treinador);
     }
 
     // Getters
@@ -29,13 +34,20 @@ public class Pokemon {
     public String getTipoSecundario() { return tipoSecundario; }
     public int getNivel() { return nivel; }
     public int getHpMaximo() { return hpMaximo; }
+    public int getFK_Treinador() {
+        return FK_Treinador;
+    }
+
+    
 
     // Setters com validação (implementadas aqui!)
     public void setId(int id) { this.id = id; }
     public void setNome(String nome) { this.nome = nome; }
     public void setTipoPrimario(String tipoPrimario) { this.tipoPrimario = tipoPrimario; }
     public void setTipoSecundario(String tipoSecundario) { this.tipoSecundario = tipoSecundario; }
-
+    public void setFK_Treinador(int FK_Treinador) {
+        this.FK_Treinador = FK_Treinador;
+    }
     public void setNivel(int nivel) {
         if (nivel < 1 || nivel > 100) {
             throw new IllegalArgumentException("O Nível do Pokémon deve estar entre 1 e 100.");
